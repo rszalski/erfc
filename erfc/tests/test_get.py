334c852e616c036e1eefa0e96c4a65f1ba72cf34
Radosław Szalski <radoslaw.szalski@gmail.com>
2014-04-06 11:06:07 +0200

    Adds some corner cases tests.

--- erfc/tests/test_get.py
+++ erfc/tests/test_get.py
@@ -8,8 +8,11 @@ from erfc.get import parse_rfc_numbers
 
 
 class TestParseRfcNumbers():
-    def test_empty_numbers(self):
+    def test_corner_cases(self):
         assert [] == parse_rfc_numbers([])
+        assert [402] == parse_rfc_numbers(['402'])
+        assert [200, 201, 202] == parse_rfc_numbers(['200-202'])
+        assert [200, 201, 202, 203] == parse_rfc_numbers(['200-202', '203'])
 
     def test_various_ranges(self):
         arg_numbers = ['100', '101', '102-104', '105', '110-112']
