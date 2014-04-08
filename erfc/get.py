a08a5769758d91b8754b32951ef2564e06cd3472
Radosław Szalski <radoslaw.szalski@gmail.com>
2014-04-08 22:51:33 +0200

    PEP8 Fixes.

--- erfc/get.py
+++ erfc/get.py
@@ -67,6 +67,7 @@ def normalize_newlines(data):
 
     return ''.join(new_text)
 
+
 def postprocess_text(data):
     '''
     Doing the postprocessing such as removing multiple spaces and so on.
@@ -76,73 +77,69 @@ def postprocess_text(data):
     # TODO: Change '-    ' to '' and '     ' to ' '
 
     new_text = []
-    
+
     for line in data:
-        #DAT DAZNT ŁORK! 
-        #line = re.sub(r'(?<!^)[ ]+',' ',line)
+        # DAT DAZNT ŁORK!
+        # line = re.sub(r'(?<!^)[ ]+',' ',line)
         new_text.append(line)
 
     return ''.join(new_text)
+
 
 def format_document(data):
     '''
     Tries to analyze type of each line and make a more aware decisions.
 
-    This approach tries analyze each line of file and choose if removing 
-    linebreaks is reasonable. 
+    This approach tries analyze each line of file and choose if removing
+    linebreaks is reasonable.
     '''
-
     data = data.splitlines(True)
     new_text = []
-   
-    #Variable useful when deleting lines
+
+    # Variable useful when deleting lines
     lines_to_del = 0
 
-    #Variable storing the state of parser (ex. paragraph, enumeration, equation etc.)
+    # Variable storing the state of parser (ex. paragraph, enumeration,
+    # equation etc.)
     state = None
 
-    #Often used regexps - compiling them should make it a bit quicker.
+    # Often used regexps - compiling them should make it a bit quicker.
     re_table_of_content = re.compile(r'\.{5}?[ ]+[0-9]')
     re_page = re.compile(r'\[Page [0-9]+\]')
     re_start_of_paragraph = re.compile(r'^[ ]{3,9}[A-Z][a-z]|^[ ]{3,9}A[ ]|^[ ]{3,9}PNG')
-    
+
     for line in data:
         if lines_to_del:
-            lines_to_del-=1
+            lines_to_del -= 1
             continue
-
         elif line[0] == '\n':
             state = None
             new_text.append(line)
-
         elif state:
             if state == 'paragraph':
                 line = re.sub(r'\n|\r\n|\r*', '', line)
                 new_text.append(line)
-
         elif re_table_of_content.search(line):
             new_text.append(line)
-
         elif re_page.search(line):
-            lines_to_del=3
-
+            lines_to_del = 3
         elif re_start_of_paragraph.search(line):
             state = 'paragraph'
             line = re.sub(r'\n|\r\n|\r*', '', line)
             new_text.append(line)
-    
         else:
             new_text.append(line)
-    
+
     new_test = postprocess_text(new_text)
 
     return ''.join(new_text)
-    
+
+
 def write_rfc(data, number, path):
     filename = 'rfc{}.{}'.format(number, RFC_FORMAT)
     rfc_path = os.path.join(path, filename)
     rfc_dir = os.path.dirname(rfc_path)
-    #data = normalize_newlines(data)
+    # data = normalize_newlines(data)
     data = format_document(data)
 
     if not os.path.exists(rfc_dir):
